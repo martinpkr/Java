07f27edb29bd0579e610c02b665fa6fc3c658369
martin.kirilov <devilmarti@abv.bg>
2023-07-16 22:06:07 +0300

    getting to know the syntax better

---
+++ ControlFlow/src/Main.java
@@ -0,0 +1,28 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 1;
+
+        while(count <= 5){
+            try{
+                System.out.println("Write number " + count + ": ");
+                String numberInput = scanner.nextLine();
+                int parsedInput = Integer.parseInt(numberInput);
+                count += 1;
+
+            }catch (Exception e){
+                System.out.println("Invalid input!");
+            }
+        }
+
+
+
+
+    }
+
+
+}
---
+++ ControlFlow/src/minMax.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class minMax {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isNum = true;
+        int count = 0;
+        int savedNumberMax = 0;
+        int savedNumberMin = 0;
+        while(isNum){
+
+            try {
+                System.out.println("Give me a number: ");
+                String inputNumber = scanner.nextLine();
+                int parsedStringToNumber = Integer.parseInt(inputNumber);
+                if(count == 0){
+                    savedNumberMin = parsedStringToNumber;
+                }
+                if (parsedStringToNumber > savedNumberMax){
+                    savedNumberMax = parsedStringToNumber;
+                }
+                if (parsedStringToNumber < savedNumberMin){
+                    savedNumberMin = parsedStringToNumber;
+                }
+                System.out.println("The lowest number for now is: " + savedNumberMin + " and the highest number is " + savedNumberMax + "the current number you typed was: " + parsedStringToNumber);
+                count += 1;
+            }catch (Exception e){
+                System.out.println("Not a Number");
+                isNum = false;
+            }
+        }
+    }
+}
